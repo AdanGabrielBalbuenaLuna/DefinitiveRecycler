dcc79fcaa5c7e416828880cb7bf0ee519fe4d4ff
Gabriel Balbuena <agbalbuenaluna@gmail.com>
2025-08-29 01:56:19 -0600

    [RECYCLER-ADD-LISTENER-IN-VIEW-HOLDER][VIEW-&-ITEM-EX] This commit contains the easy way to add a listener in a item inside a recycler view, for this example listener is inside the image view and the hole item

--- app/src/main/java/com/example/definitiverecycler/adapter/SuperHeroViewHolder.kt
+++ app/src/main/java/com/example/definitiverecycler/adapter/SuperHeroViewHolder.kt
@@ -1,6 +1,7 @@
 package com.example.definitiverecycler.adapter
 
 import android.view.View
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.definitiverecycler.SuperHero
@@ -15,5 +16,16 @@ class SuperHeroViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         binding.tvRealName.text = superHeroModel.realName
         binding.tvPublisher.text = superHeroModel.publisher
         Glide.with(binding.ivSuperHero.context).load(superHeroModel.photo).into(binding.ivSuperHero)
+        binding.ivSuperHero.setOnClickListener {
+            Toast.makeText(binding.ivSuperHero.context, superHeroModel.realName, Toast.LENGTH_SHORT).show()
+        }
+
+        itemView.setOnClickListener {
+            Toast.makeText(
+                binding.ivSuperHero.context,
+                superHeroModel.superhero,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
     }
 }
